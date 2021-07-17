c0e1aafa49b9e43e2fc185a1350f77e75085610c
Milosz Kulesza <milosz.kulesza1@gmail.com>
2021-07-17 22:18:21 +0200

    splash screen

---
+++ WMSBarcodeScanner/WMSBarcodeScanner.Android/SplashScreenActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using AndroidX.AppCompat.App;
+using System.Threading.Tasks;
+
+namespace WMSBarcodeScanner.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScreenActivity : AppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(SplashScreenActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { Startup(); });
+            startupWork.Start();
+        }
+
+        private void Startup()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+
+        public override void OnBackPressed() { }
+    }
+}
